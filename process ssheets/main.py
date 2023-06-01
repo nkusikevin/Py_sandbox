3706db0a9ced293b289ed43c4ba64dc0d063a4c7
nkusikevin <nkusikvnhart@gmail.com>
2023-06-01 23:36:44 +0200

    adding sheet processor

--- process ssheets/main.py
+++ process ssheets/main.py
@@ -1,4 +1,5 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook("transactions.xlsx")
 
@@ -12,5 +13,8 @@ for row in range(2,sheet.max_row +1) :
     corrected_price_cell = sheet.cell(row,4)
     corrected_price_cell.value = corrected_price
 
-
+values = Reference(sheet, min_row=2, max_row=sheet.max_row , min_col=4, max_col=4)
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, "e2")
 wb.save("transactions(copy).xlsx")
